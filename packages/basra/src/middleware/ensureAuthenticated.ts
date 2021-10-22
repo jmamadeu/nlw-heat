67800e842e7c62e9100268abccd53818d5b20c16
João Amadeu <jmamadeu2000@gmail.com>
2021-10-22 16:39:27 +0100

    feat: add token verification

---
+++ packages/basra/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: 'token.invalid',
+    });
+  }
+
+  const [, token] = authToken.split(' ');
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET as string);
+
+    request.user_id = sub as string;
+
+    next();
+  } catch (err: any) {
+    return response.status(401).json({
+      errorCode: err.message as string,
+    });
+  }
+}
