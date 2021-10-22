76b217b7e10bf13080444650a1a92581bd6d99e8
João Amadeu <jmamadeu2000@gmail.com>
2021-10-22 18:19:51 +0100

    feat: user profile

---
+++ packages/basra/src/controllers/ProfileUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { ProfileUserService } from '../services/ProfileUserService';
+
+class ProfileUserController {
+  async handle(request: Request, response: Response) {
+    const service = new ProfileUserService();
+
+    const user = await service.execute(request.user_id);
+
+    return response.json(user);
+  }
+}
+
+export { ProfileUserController };
--- packages/basra/src/routes.ts
+++ packages/basra/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLastThreeMessagesController } from './controllers/GetLastThreeMessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 import { GetLastThreeMessagesService } from './services/GetLastThreeMessagesService';
 
@@ -16,6 +17,8 @@ router.post(
 );
 
 router.get('/messages/last3', new GetLastThreeMessagesController().handle);
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
 
 router.get('/signin/callback', (request, response) => {
   const { code } = request.query;
