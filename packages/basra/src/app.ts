1ed30cfe323708dd37bc8de8351d06c5a9b72af8
João Amadeu <jmamadeu2000@gmail.com>
2021-10-22 17:40:03 +0100

    chore: configure socket.io

--- packages/basra/src/app.ts
+++ packages/basra/src/app.ts
@@ -1,12 +1,28 @@
 import 'dotenv/config';
 
-import express from 'express';
+import express, { response } from 'express';
+import cors from 'cors';
+import http from 'http';
 
 import { router } from './routes';
+import { Server } from '../../../node_modules/socket.io/dist';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log('server is running at port 3333'));
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('new connection', socket.id, 'teste');
+});
+
+export { httpServer, io };
